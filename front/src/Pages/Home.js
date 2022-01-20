e62036d13721b32252bd8b4035581b04cd086ae4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-20 22:06:29 +0530

    note file added

--- front/src/Pages/Home.js
+++ front/src/Pages/Home.js
@@ -1,17 +1,21 @@
 import React from "react";
+import HoCss from "./CSS/Home.module.css";
 import Search from "./../Components/Search/Search";
 import WatchList from "./../Components/WatchList/WatchList";
 import PieChart from "./../Components/PieChart/PieChart";
-
+import Note from "./../Components/Note/Note"
 export default function Home() {
     return (
-        <div>
+        <div className={HoCss.Hmdiv}>
             <div>
                 <Search />
                 <WatchList />
             </div>
             <div>
                 <PieChart />
+            </div>
+            <div>
+                <Note />
             </div>
         </div>
     );
