62494ad2ef35d0059397ff87abd8208f42ef0133
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-20 23:15:40 +0530

    css updated

--- front/src/Components/Note/Note.js
+++ front/src/Components/Note/Note.js
@@ -1,6 +1,30 @@
 import React from "react";
+import JSONDATA from "./../../Data/NoteList.json";
 import "./CSS/Note.css";
+import AddIcon from '@material-ui/icons/Add';
 
 export default function Note() {
-    return <div>Note</div>;
+    return <div id="NotemTDiv">
+        <div className="NoteTDiv">
+            <p>Note</p>
+            <div>
+                <AddIcon />
+            </div>
+        </div>
+        <div>
+            {JSONDATA.map((value, key) => {
+                return (
+                    <div key={key} className="NMapDiv">
+                        <div className="TitleDiv">
+                            <p className="pTagT">{value.title}</p>
+                            <p>{value.date}</p>
+                        </div>
+                        <div>
+                            {value.des}
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    </div>;
 }
