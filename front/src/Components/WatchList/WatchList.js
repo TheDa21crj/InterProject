d59d3df34358555f64df1f6cc161d5d89f885b70
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-20 22:23:28 +0530

    adding css

--- front/src/Components/WatchList/WatchList.js
+++ front/src/Components/WatchList/WatchList.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
+import "./CSS/WatchList.css";
 import JSONDATA from "./../../Data/showList.json";
 
 export default function WatchList() {
-    const getData = async function (e) {
-        const response = await fetch('http://localhost:5000/api/showwatchList');
-        const data = await response.json();
-        console.log(typeof data);
-    }
-    return <div onClick={getData}>
-        <div>
-            {JSONDATA.map((value, key) => {
-                return (
-                    <div>
-                        <div key={key}>
-                            <p type="text" >{value.name}</p>
-                        </div>
-                    </div>
-                )
-            })}
-            <div id="container"></div>
-        </div>
-    </div>;
+  const getData = async function (e) {
+    const response = await fetch("http://localhost:5000/api/showwatchList");
+    const data = await response.json();
+  };
+  return (
+    <div onClick={getData} className="WatchmDiv">
+      <div>
+        {JSONDATA.map((value, key) => {
+          return (
+            <div>
+              <div key={key}>
+                <p clcasName="pTagVal">{value.name}</p>
+              </div>
+            </div>
+          );
+        })}
+        <div id="container"></div>
+      </div>
+    </div>
+  );
 }
