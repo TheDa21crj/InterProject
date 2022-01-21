a4131b56a6d991a1514cadd9bb54347355f72ffb
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-22 02:43:52 +0530

    data base working 100%

--- router/Rules.js
+++ router/Rules.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const router = express.Router();
 const fs = require("fs");
+const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const Connect = require("../module/Rules");
 
