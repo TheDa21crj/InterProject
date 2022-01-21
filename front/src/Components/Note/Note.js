502decb3c210493e0d64db651623b0fc73b56273
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-21 08:18:50 +0530

    note toggle added

--- front/src/Components/Note/Note.js
+++ front/src/Components/Note/Note.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import JSONDATA from "./../../Data/NoteList.json";
 import "./CSS/Note.css";
 import AddIcon from '@material-ui/icons/Add';
 
 export default function Note() {
+    const [show, setShow] = useState(false);
+
+    const postData = async function (e) {
+        setShow(false);
+        const title = document.getElementById('inpTag').value;
+        const des = document.getElementById('TextTag').value;
+        try {
+            await fetch("http://localhost:5000");
+            const r = await fetch("/api/notemaker", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    title,
+                    des
+                }),
+            });
+            const data = await r.json();
+            if (!data.status) {
+                console.log("Data Poster");
+            } else {
+                console.log("Not");
+            }
+        } catch (error) {
+            console.log(error);
+            getData(e);
+        }
+    }
+
+    const getData = async function (e) {
+        const response = await fetch("http://localhost:5000/api/notemaker/show");
+        const data = await response.json();
+    };
     return <div id="NotemTDiv">
         <div className="NoteTDiv">
             <p>Note</p>
             <div>
-                <AddIcon />
+                <AddIcon onClick={() => setShow(!show)} className="addIcons" />
             </div>
         </div>
         <div>
             {JSONDATA.map((value, key) => {
                 return (
-                    <div key={key} className="NMapDiv">
+                    <div key={key} className="NMapDiv" onClick={getData}>
                         <div className="TitleDiv">
                             <p className="pTagT">{value.title}</p>
                             <p>{value.date}</p>
@@ -25,6 +59,23 @@ export default function Note() {
                     </div>
                 )
             })}
+            {
+                show ? (<div className="modalMDiv">
+                    <div className="popDiv">
+                        <form className="form">
+                            <input type="text" name="" id="inpTag" />
+                            <textarea name="" cols="30" rows="10" id="TextTag"></textarea>
+                        </form>
+                        <div className="DivBtn">
+                            <button id="BtnClose" onClick={() => setShow(false)}>Close</button>
+                            <button id="AddBtn" onClick={(e) => {
+                                postData(e);
+                            }}>Add</button>
+                        </div>
+                    </div>
+                </div>) : null
+            }
+
         </div>
     </div>;
 }
