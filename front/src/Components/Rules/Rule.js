b0c13655cbc88bd9b5f7b9099b94d5d36e75e5ec
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-22 02:16:20 +0530

    rules head icons added

--- front/src/Components/Rules/Rule.js
+++ front/src/Components/Rules/Rule.js
@@ -1,5 +1,18 @@
 import React from "react";
+import "./CSS/Rules.css";
+import tickIcon from "./../../Req img/tick.jpg";
+import addIcon from "./../../Req img/add.jpg";
+import deleteIcon from "./../../Req img/bin.jpg";
 
 export default function Rule() {
-  return <div>Rule</div>;
+    return <div className="RmDiv">
+        <div className="iconsTile">
+            <div>Rules</div>
+            <div>
+                <img src={tickIcon} alt="" id="RtickIcon" />
+                <img src={addIcon} alt="" id="RaddIcon" />
+                <img src={deleteIcon} alt="" id="RdeleteIcon" />
+            </div>
+        </div>
+    </div>;
 }
