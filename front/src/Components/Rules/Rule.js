1009943ce3fef727dc0a5dedeef8a2271957de3d
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-22 09:47:57 +0530

    working on add rule

--- front/src/Components/Rules/Rule.js
+++ front/src/Components/Rules/Rule.js
@@ -1,18 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import JSONDATA from "./../../Data/Showrule.json";
 import "./CSS/Rules.css";
 import tickIcon from "./../../Req img/tick.jpg";
 import addIcon from "./../../Req img/add.jpg";
 import deleteIcon from "./../../Req img/bin.jpg";
 
 export default function Rule() {
-    return <div className="RmDiv">
-        <div className="iconsTile">
-            <div>Rules</div>
-            <div>
-                <img src={tickIcon} alt="" id="RtickIcon" />
-                <img src={addIcon} alt="" id="RaddIcon" />
-                <img src={deleteIcon} alt="" id="RdeleteIcon" />
+    const [show, setShow] = useState(false);
+
+    const getData = async function (e) {
+        const response = await fetch("http://localhost:5000/api/rules/show");
+        const data = await response.json();
+    };
+
+    const postData = async function (e) {
+        const rule = document.getElementById("textAdd").value;
+        console.log(rule);
+        try {
+            await fetch("http://localhost:5000");
+            const r = await fetch("/api/rules", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    rule,
+                }),
+            });
+            const data = await r.json();
+            if (!data.status) {
+                console.log("Data Poster");
+            } else {
+                console.log("Not");
+            }
+        } catch (error) {
+            console.log(error);
+            getData(e);
+        }
+    }
+    return (
+        <div onClick={getData} className="RmDiv">
+            <div className="iconsTile">
+                <div>Rules</div>
+                <div>
+                    <img src={tickIcon} alt="" id="RtickIcon" />
+                    <img src={addIcon} alt="" id="RaddIcon" onClick={() => setShow(!show)} />
+                    <img src={deleteIcon} alt="" id="RdeleteIcon" />
+                </div>
             </div>
+            <div>
+                {JSONDATA.map((value, key) => {
+                    return (
+                        <div>
+                            <input type="radio" name="" className="ckeckIt" />
+                            <form action="">
+                                <input type="text" name="" id="" value={value.rule} />
+                            </form>
+                        </div>
+                    )
+                })}
+            </div>
+            {
+                show ? (<div className="AddmDiv">
+                    <div className="AddDiv">
+                        <form method="POST" className="AddmForm">
+                            <input type="text" name="" value="Rules" id="AddValue" />
+                            <textarea name="" id="textAdd" cols="30" rows="10"></textarea>
+                        </form>
+                        <div className="AddBtnDiv">
+                            <button id="RbtnClose" onClick={() => setShow(false)}>Close</button>
+                            <button id="RbtnAdd" onClick={postData}>Add</button>
+                        </div>
+                    </div>
+                </div>) : null
+            }
         </div>
-    </div>;
+    );
 }
