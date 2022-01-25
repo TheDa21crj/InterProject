f7fd264081d8080092245ca47b9d0348c8d54644
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-25 10:26:28 +0530

    add values in table

--- front/src/Components/Table/CTable.js
+++ front/src/Components/Table/CTable.js
@@ -5,9 +5,25 @@ export default function CTable(props) {
         <p>{props.TableName}</p>
         <table border="1" className="table">
             <tr>
-                <th>Company</th>
-                <th>Contact</th>
-                <th>Country</th>
+                <th><input type="checkbox" value="" /></th>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+            </tr> <tr>
+                <th><input type="checkbox" value="" /></th>
+                <th>{props.product1}</th>
+                <th>{props.quantity1}</th>
+                <th>priceChange</th>
+            </tr> <tr>
+                <th><input type="checkbox" value="" /></th>
+                <th>{props.product2}</th>
+                <th>{props.quantity2}</th>
+                <th>priceChange</th>
+            </tr> <tr>
+                <th><input type="checkbox" value="" /></th>
+                <th>{props.product3}</th>
+                <th>{props.quantity3}</th>
+                <th>priceChange</th>
             </tr>
         </table>
     </div>;
--- front/src/Components/Table/Table.js
+++ front/src/Components/Table/Table.js
@@ -4,7 +4,13 @@ import "./CSS/Table.css";
 
 export default function Table() {
     return <div id="TmDiv">
-        <CTable TableName="Fruits" />
-        <CTable TableName="Vegetables" />
+        <CTable TableName="Fruits"
+            product1="Apples"
+            product2="Banana"
+            product3="Mango" />
+        <CTable TableName="Vegetables"
+            product1="Potatos"
+            product2="Tomatoes"
+            product3="Onions" />
     </div>;
 }
