14b2b44af5cbec17873355ccbd118ce13740b369
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-25 11:11:41 +0530

    price changing every sec

--- front/src/Components/Table/CTable.js
+++ front/src/Components/Table/CTable.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 export default function CTable(props) {
+    const [price1, setprice1] = useState(100);
+    const [price2, setprice2] = useState(80);
+    const [price3, setprice3] = useState(100);
+    const totalPrice = useRef();
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setprice1(Math.floor(Math.random() * (120 - 20 + 1)) + 20);
+            setprice2(Math.floor(Math.random() * (120 - 20 + 1)) + 20);
+            setprice3(Math.floor(Math.random() * (120 - 20 + 1)) + 20);
+        }, 1000);
+    }, [])
+
+    const Total = async function (e) {
+        console.log(totalPrice.current.innerHTML);
+    }
+
     return <div >
         <p>{props.TableName}</p>
         <table border="1" className="table">
@@ -9,22 +25,26 @@ export default function CTable(props) {
                 <th>Product</th>
                 <th>Quantity</th>
                 <th>Price</th>
-            </tr> <tr>
+            </tr>
+            <tr>
                 <th><input type="checkbox" value="" /></th>
                 <th>{props.product1}</th>
                 <th>{props.quantity1}</th>
-                <th>priceChange</th>
-            </tr> <tr>
+                <th>{price1}</th>
+            </tr>
+            <tr>
                 <th><input type="checkbox" value="" /></th>
                 <th>{props.product2}</th>
                 <th>{props.quantity2}</th>
-                <th>priceChange</th>
-            </tr> <tr>
+                <th>{price2}</th>
+            </tr>
+            <tr>
                 <th><input type="checkbox" value="" /></th>
                 <th>{props.product3}</th>
-                <th>{props.quantity3}</th>
-                <th>priceChange</th>
+                <th onClick={Total}>{props.quantity3}</th>
+                <th>{price3}</th>
             </tr>
+            <p>Total <span ref={totalPrice}>500</span></p>
         </table>
     </div>;
 }
--- front/src/Components/Table/Table.js
+++ front/src/Components/Table/Table.js
@@ -7,10 +7,16 @@ export default function Table() {
         <CTable TableName="Fruits"
             product1="Apples"
             product2="Banana"
-            product3="Mango" />
+            product3="Mango"
+            quantity1="500"
+            quantity2="50"
+            quantity3="20" />
         <CTable TableName="Vegetables"
             product1="Potatos"
             product2="Tomatoes"
-            product3="Onions" />
+            product3="Onions"
+            quantity1="300"
+            quantity2="50"
+            quantity3="40" />
     </div>;
 }
