109cd0c1a059efa9fe387c5a71cff7f501a3997e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-26 09:48:51 +0530

    edit styled

--- front/src/Components/Note/Edit.js
+++ front/src/Components/Note/Edit.js
@@ -1,22 +1,62 @@
-import React from "react";
-import "./CSS/Note.css"
+import React, { useState } from "react";
+import "./CSS/Edit.css"
 
 export default function Edit(props) {
+  const [showDes, setDes] = useState(props.valD);
+
+  const setData = async function (e) {
+    setDes(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const postData = async function (e) {
+    props.idM(null);
+    const title = props.valNote;
+    const des = showDes;
+    try {
+      await fetch("http://localhost:5000");
+      const r = await fetch("/api/notemaker/update", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          des,
+        }),
+      });
+      const data = await r.json();
+      if (!data.status) {
+        console.log("Data Poster");
+      } else {
+        console.log("Not");
+      }
+    } catch (error) {
+      console.log(error);
+      getData(e);
+    }
+  };
+
+  const getData = async function (e) {
+    const response = await fetch("http://localhost:5000/api/notemaker/show");
+    const data = await response.json();
+  };
   return (
-    <div>
-      <div>
-        <form method="GET">
-          <input type="text" name="" id="" value={props.valNote} />
-          <textarea type="text" name="" id="" >{props.valD}</textarea>
+    <div className="EpopEdit">
+      <div className="EeditDiv">
+        <form method="PUT" className="EditformDiv">
+          <input type="text" name="" id="valNote" value={props.valNote} />
+          <textarea type="text" name="" id="showDes" onChange={setData}>{showDes}</textarea>
         </form>
       </div>
-      <div>
+      <div className="EditbtnDiv">
         <button
+          id="editCloseBtn"
           onClick={() => {
             props.idM(null);
           }}
         >Close</button>
-        <button>Add</button>
+        <button onClick={postData} id="editAddBtn">Add</button>
       </div>
     </div>
   );
--- router/NoteMaker.js
+++ router/NoteMaker.js
@@ -54,20 +54,6 @@ router.delete(
   }
 );
 
-router.get("/find", [check("_id", "id is required")], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  const { _id } = req.body;
-  const data = await Connect.findOne({ _id: _id });
-  fs.writeFile("./front/src/Data/Data.json", JSON.stringify(data), (err) => {
-    console.log("done");
-    console.log(err);
-  });
-  res.json({ message: data });
-});
-
 router.put(
   "/update",
   [check("title", "title is required")],
