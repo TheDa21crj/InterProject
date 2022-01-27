e36ee255edde7b7127809d443bdd19a59ed5f73e
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-01-27 09:16:14 +0530

    adding data

--- front/src/Components/PieChart/PieChart.js
+++ front/src/Components/PieChart/PieChart.js
@@ -2,25 +2,25 @@ import React from "react";
 import "./CSS/PieChart.css";
 
 export default function PieChart() {
-    return (
-        <div className="PieChartmDiv">
-            <div className="pie-chart">
-                <div className="centerWhite"></div>
-            </div>
-            <div className="chatKey">
-                <p>
-                    <span id="item1"></span>Item 1 - 35%
-                </p>
-                <p>
-                    <span id="item2"></span>Item 2 - 25%
-                </p>
-                <p>
-                    <span id="item3"></span>Item 3 - 15%
-                </p>
-                <p>
-                    <span id="item4"></span>Item 4 - 25%
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="PieChartmDiv">
+      <div className="pie-chart">
+        <div className="centerWhite"></div>
+      </div>
+      <div className="chatKey">
+        <p>
+          <span id="item1"></span>Item 1 - 35%
+        </p>
+        <p>
+          <span id="item2"></span>Item 2 - 25%
+        </p>
+        <p>
+          <span id="item3"></span>Item 3 - 15%
+        </p>
+        <p>
+          <span id="item4"></span>Item 4 - 25%
+        </p>
+      </div>
+    </div>
+  );
 }
