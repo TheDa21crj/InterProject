f32828d1ce436d8c779e9f1f0e85491ecd28ce15
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-07 18:53:17 +0530

    adding nav

---
+++ front/src/Components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NCss from "./CSS/Nav.module.css";
+
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+
+export default function Nav() {
+  return (
+    <div className={NCss.NmDiv}>
+      <ArrowBackIosIcon fontSize="large" />
+      <h1>The Da 's Trading</h1>
+      <ArrowForwardIosIcon fontSize="large" />
+    </div>
+  );
+}
--- front/src/Pages/Home.js
+++ front/src/Pages/Home.js
@@ -5,20 +5,24 @@ import PieChart from "./../Components/PieChart/PieChart";
 import Note from "./../Components/Note/Note";
 import Rule from "./../Components/Rules/Rule";
 import Table from "./../Components/Table/Table";
+import Nav from "./../Components/Nav/Nav";
 
 export default function Home() {
   return (
-    <div className={HoCss.Hmdiv}>
-      <div>
-        <Search />
-      </div>
-      <div>
-        <PieChart />
-        <Table />
-      </div>
-      <div>
-        <Note />
-        <Rule />
+    <div>
+      <Nav />
+      <div className={HoCss.Hmdiv}>
+        <div>
+          <Search />
+        </div>
+        <div>
+          <PieChart />
+          <Table />
+        </div>
+        <div>
+          <Note />
+          <Rule />
+        </div>
       </div>
     </div>
   );
