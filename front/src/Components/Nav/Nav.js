fb4a11e415e2f55729c2895f54ed87d58dc21a86
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-10 10:33:15 +0530

    working on sticky nav

--- front/src/Components/Nav/Nav.js
+++ front/src/Components/Nav/Nav.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NCss from "./CSS/Nav.module.css";
-
+import "./CSS/Nav.css";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 export default function Nav() {
+  const [nav, setnav] = useState(false);
+
+  // const changeNav = () => {
+  //   if (window.scrollY >= 16) {
+  //     setnav(true);
+  //   } else {
+  //     setnav(false);
+  //   }
+  // };
+
+  // const changeNav = (e) => {
+  //   console.log(window.scrollY);
+  // };
+
+  // window.addEventListener("scroll", changeNav);
   return (
-    <div className={NCss.NmDiv}>
+    <div className={NCss.NmDiv} id={nav ? "active" : ""}>
       <div className={NCss.mcontent}>
         <ArrowBackIosIcon fontSize="large" id={NCss.ArrowBackIosIcon} />
         <Link to="/" className="Link">
