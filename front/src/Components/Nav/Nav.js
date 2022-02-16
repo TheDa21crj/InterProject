aede7486adcc933155ac128343635fca92e297bd
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-16 17:23:30 +0530

    Update Nav.js

--- front/src/Components/Nav/Nav.js
+++ front/src/Components/Nav/Nav.js
@@ -6,23 +6,15 @@ import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 export default function Nav() {
-    return ( <
-        div className = { NCss.NmDiv } >
-        <
-        div className = { NCss.mcontent } >
-        <
-        ArrowBackIosIcon fontSize = "large"
-        id = { NCss.ArrowBackIosIcon }
-        />{" "} <
-        Link to = "/"
-        className = "Link" >
-        <
-        h1 className = { NCss.mH1 } > The Da 's Trading</h1>{" "} <
-        /Link>{" "} <
-        ArrowForwardIosIcon fontSize = "large"
-        id = { NCss.ArrowForwardIosIcon }
-        />{" "} <
-        /div>{" "} <
-        /div>
-    );
+  return (
+    <div className={NCss.NmDiv}>
+      <div className={NCss.mcontent}>
+        <ArrowBackIosIcon fontSize="large" id={NCss.ArrowBackIosIcon} />{" "}
+        <Link to="/" className="Link">
+          <h1 className={NCss.mH1}> The Da 's Trading</h1>{" "}
+        </Link>{" "}
+        <ArrowForwardIosIcon fontSize="large" id={NCss.ArrowForwardIosIcon} />{" "}
+      </div>{" "}
+    </div>
+  );
 }
