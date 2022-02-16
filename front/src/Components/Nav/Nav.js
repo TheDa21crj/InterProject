e343af85fc3ad25fdee861f96517c4f7dc3acd8f
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-16 17:23:16 +0530

    Update Nav.js

--- front/src/Components/Nav/Nav.js
+++ front/src/Components/Nav/Nav.js
@@ -6,30 +6,23 @@ import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 export default function Nav() {
-  const [nav, setnav] = useState(false);
-
-  // const changeNav = () => {
-  //   if (window.scrollY >= 16) {
-  //     setnav(true);
-  //   } else {
-  //     setnav(false);
-  //   }
-  // };
-
-  // const changeNav = (e) => {
-  //   console.log(window.scrollY);
-  // };
-
-  // window.addEventListener("scroll", changeNav);
-  return (
-    <div className={NCss.NmDiv} id={nav ? "active" : ""}>
-      <div className={NCss.mcontent}>
-        <ArrowBackIosIcon fontSize="large" id={NCss.ArrowBackIosIcon} />
-        <Link to="/" className="Link">
-          <h1 className={NCss.mH1}>The Da 's Trading</h1>
-        </Link>
-        <ArrowForwardIosIcon fontSize="large" id={NCss.ArrowForwardIosIcon} />
-      </div>
-    </div>
-  );
+    return ( <
+        div className = { NCss.NmDiv } >
+        <
+        div className = { NCss.mcontent } >
+        <
+        ArrowBackIosIcon fontSize = "large"
+        id = { NCss.ArrowBackIosIcon }
+        />{" "} <
+        Link to = "/"
+        className = "Link" >
+        <
+        h1 className = { NCss.mH1 } > The Da 's Trading</h1>{" "} <
+        /Link>{" "} <
+        ArrowForwardIosIcon fontSize = "large"
+        id = { NCss.ArrowForwardIosIcon }
+        />{" "} <
+        /div>{" "} <
+        /div>
+    );
 }
