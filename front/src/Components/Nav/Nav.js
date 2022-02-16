9fa1da2729c4e6c23eda87cde2764b348b44be6b
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-16 17:23:49 +0530

    Update Nav.js

--- front/src/Components/Nav/Nav.js
+++ front/src/Components/Nav/Nav.js
@@ -9,12 +9,12 @@ export default function Nav() {
   return (
     <div className={NCss.NmDiv}>
       <div className={NCss.mcontent}>
-        <ArrowBackIosIcon fontSize="large" id={NCss.ArrowBackIosIcon} />{" "}
+        <ArrowBackIosIcon fontSize="large" id={NCss.ArrowBackIosIcon} />
         <Link to="/" className="Link">
-          <h1 className={NCss.mH1}> The Da 's Trading</h1>{" "}
-        </Link>{" "}
-        <ArrowForwardIosIcon fontSize="large" id={NCss.ArrowForwardIosIcon} />{" "}
-      </div>{" "}
+          <h1 className={NCss.mH1}> The Da 's Trading</h1>
+        </Link>
+        <ArrowForwardIosIcon fontSize="large" id={NCss.ArrowForwardIosIcon} />
+      </div>
     </div>
   );
 }
