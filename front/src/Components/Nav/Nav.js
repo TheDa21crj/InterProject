4b511a248611a67f8a1422f7458c38d707bc5974
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-02-16 17:53:50 +0530

    useEffect import

--- front/src/Components/Nav/Nav.js
+++ front/src/Components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NCss from "./CSS/Nav.module.css";
 import "./CSS/Nav.css";
